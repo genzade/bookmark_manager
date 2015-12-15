6370fad8ca2d5c8e52b0239d19c0dd9dd5da945a
giamir <giamir.buoncristiani@gmail.com>
2015-12-15 12:55:42 +0000

    setup sorted

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,6 +1,10 @@
-feature 'show a list of links' do
-  scenario 'shows a list of links in homepage' do
-    Link.create(title: 'Blog of Giamir', link: 'http://giamir.com')
-    expect(page).to have_content ('Blog of Giamir')
+feature 'Viewing links' do
+  scenario 'I can see existing links on the links page' do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    visit '/links'
+    expect(page.status_code).to eq 200
+    within 'ul#links' do
+      expect(page).to have_content('Makers Academy')
+    end
   end
 end
